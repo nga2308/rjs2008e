367ab6ff386ec61459371a6dd6f93ba094b0407c
nga2308 <67799422+nga2308@users.noreply.github.com>
2020-10-18 20:53:13 +0700

    ex8

---
+++ javascript-part/ex_JS/dom/dom.js
@@ -0,0 +1,11 @@
+//---------6-------------
+// var green = document.querySelector("div#ex6 button.js-bgr-blue");
+// var red = document.querySelector("div#ex6 button.js-bgr-red");
+// var brg = document.getElementById("ex6");
+// green.addEventListener("click", function () {
+//   brg.style.backgroundColor = "green";
+// });
+// red.addEventListener("click", function () {
+//   brg.style.backgroundColor = "red";
+// });
+//--------------10---------------

0c859c5265f422d53a3cf55109dbf2ffab27cdfc
nga2308 <67799422+nga2308@users.noreply.github.com>
2020-11-11 10:40:14 +0700

    daysix

--- javascript-part/daysix/index.js
+++ javascript-part/daysix/index.js
@@ -1,14 +1,17 @@
-// var h3 = document.getElementById("h3");
-var parent = document.getElementById("parent");
-var h3_2 = document.getElementsByTagName("h3")[0];
-console.log("h3", h3);
+// var h3 = document.getElementById("h3")
 
-// h3.style.color = "red";
-h3_2.style.color = "blue";
-h3_2.style.backgroundColor = "yellow";
+var parent = document.getElementById("parent")
+var h3_2 = parent.getElementsByTagName("h3")[0]
 
-var p = document.getElementsByClassName("hello");
-p.style.color = "yellow";
-var data = "<span style='color:red'>Hello change by js</span>";
-p.innerHTML = data;
-p.innerText = data;
+console.log("h3_2", h3_2)
+
+// h3.style.color = "red"
+h3_2.style.color = "blue"
+h3_2.style.backgroundColor = "yellow"
+
+var p = document.getElementsByClassName("hello")[0]
+p.style.color = "yellow"
+
+var data = "<span style='color: red'> Hello </span> change by js <script>alert('Hello')</script>"
+// p.innerHTML = data
+p.innerText = data
